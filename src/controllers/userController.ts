62249d516105d150501e532f59173a82da3443e6
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-07 16:02:52 -0300

    feat: create user controller to register new user

---
+++ src/controllers/userController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import { userService } from '../services';
+import {
+  UserCreationData,
+  UserRequest,
+  userRequestToCreationData,
+  UserView,
+} from '../types/userTypes';
+
+export async function registerNewUser(
+  req: Request,
+  res: Response<UserView, { signupInfo: UserRequest }>,
+): Promise<Response<UserView>> {
+  const requestBody: UserRequest = res.locals.signupInfo;
+  const user: UserCreationData = userRequestToCreationData(requestBody);
+  const createdUser: UserView = await userService.createNewUser(user);
+  return res.status(201).json(createdUser);
+}
