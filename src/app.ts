ee727ce1b536dd2528d407c84b66dcee1e144d03
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-07 18:00:30 -0300

    feat: implement error middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Express } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+
+import { errorMiddleware } from './middlewares';
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+app.use(errorMiddleware.errorHandler);
 
 export default app;
