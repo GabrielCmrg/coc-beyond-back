70d2c87d6be63c92f112ff288f5c61732a7f4e3b
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-07 22:00:17 -0300

    chore: add paths to be ignored on coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,5 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['node_modules'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/src/config',
+    '<rootDir>/src/exceptions',
+    '<rootDir>/src/repositories',
+    '<rootDir>/src/types',
+    '<rootDir>/src/services/index.ts',
+  ],
 };
