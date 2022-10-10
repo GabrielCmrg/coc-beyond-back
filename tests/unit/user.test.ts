6194b88c95da30377d760522695fe17122389e57
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-10 16:05:06 -0300

    test: add user test to check if password is hashed

--- tests/unit/user.test.ts
+++ tests/unit/user.test.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 import { userService } from '../../src/services';
 import { userRepository } from '../../src/repositories';
 
@@ -40,5 +42,26 @@ describe('Create user', () => {
 
     // assert
     await expect(promise).rejects.toBeTruthy();
+  });
+
+  it('Should hash the password before create', async () => {
+    // arrange
+    const userToCreate = {
+      email: 'controll@email.com',
+      password: 'secret',
+    };
+    jest.spyOn(userRepository, 'getByEmail').mockResolvedValue(null);
+    jest
+      .spyOn(bcrypt, 'hashSync')
+      .mockImplementation((input) => input as string);
+    jest
+      .spyOn(userRepository, 'create')
+      .mockResolvedValue({ ...userToCreate, id: 1 });
+
+    // act
+    await userService.createNewUser(userToCreate);
+
+    // assert
+    expect(bcrypt.hashSync).toBeCalled();
   });
 });
